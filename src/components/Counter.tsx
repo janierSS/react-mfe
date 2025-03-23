fdd9e77c6216a4090b7dcba8a6edf1d742c62f92
Janier <janierss17@gmail.com>
2025-03-23 13:10:46 -0400

    Fix styles

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import logo from '../assets/react.svg'
-import styles from "./Counter.module.scss";
+import logo from "../assets/react.svg";
 import { eventDispatcher } from "../utils/eventManager";
+import styles from "./Counter.module.scss";
 
 type CounterProps = {
-  count: number;  
+  count: number;
 };
 
 const Counter: React.FC<CounterProps> = ({ count }) => {
-  console.log('Counter', count);
-  
+  console.log("Counter", count);
+
   return (
     <div className={styles.container}>
-      <img src={logo} alt="logo" />
-      <h1>React MFE</h1>
+      <div className={styles["container__header"]}>
+        <img src={logo} alt="logo" />
+        <h1>React MFE</h1>
+      </div>
       <p>Counter: {count}</p>
-      <button onClick={() => eventDispatcher('increment')}>Increment</button>
-      <button onClick={() => eventDispatcher('decrement')}>Decrement</button>
+      <button onClick={() => eventDispatcher("increment")}>Increment</button>
+      <button onClick={() => eventDispatcher("decrement")}>Decrement</button>
     </div>
   );
 };
